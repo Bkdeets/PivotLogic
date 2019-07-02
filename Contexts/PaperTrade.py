a6d881ab77fe27dd7f771cba153d38036b19cbf9
Britton Deets <britton.deets@ncino.com>
2019-07-02 12:44:24 -0400

    base structure for contexts

--- Contexts/PaperTrade.py
+++ Contexts/PaperTrade.py
@@ -1,2 +1,16 @@
+from Contexts.Context import Context
+
 
 # class to implement Context methods that execute in a paper trading environment
+
+
+class LiveTrade(Context):
+
+    def __init__(self):
+        super().__init__()
+
+    def buy(self, ticker, order_type, units, limit_price=None):
+        return
+
+    def sell(self, ticker, order_type, units, limit_price=None):
+        return
