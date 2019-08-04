c7f1d033ea392ec8cf65818c84ab1613a89468e7
Britton <brittonkdeets@gmail.com>
2019-08-04 12:33:10 -0400

    setting up a test strat for backtesting framework

--- Contexts/Backtest.py
+++ Contexts/Backtest.py
@@ -1,4 +1,5 @@
 from Contexts.Context import Context
+import alpaca_trade_api as tradeapi
 
 
 # class to implement Context methods that execute in a backtesting environment
@@ -18,4 +19,24 @@ class Backtest(Context):
         return
 
     def get_api(self):
+        return tradeapi.REST(
+            key_id='PKIG33U7XYR8ECVMMF4A',
+            secret_key='e83t4Cn5oY07EENvlhRKwjKyTbykd8wn8Phesmze',
+            base_url='https://paper-api.alpaca.markets')
+
+    def get_barset(self, symbols, timeframe, limit, start, end):
+        return self.get_api().get_barset(
+            symbols,
+            timeframe,
+            limit=limit,
+            start=start,
+            end=end)
+
+    def get_clock(self):
+        return
+
+    def get_account(self):
+        return
+
+    def list_positions(self):
         return
